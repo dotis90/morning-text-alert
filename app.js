0c2f37f47084aac4f4ec0e335bc0abc686012bdf
dotis90 <dotis90@gmail.com>
2021-05-05 18:25:25 -0400

    Added a module to determine the date

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const axios = require('axios');
+const date = require('./utils/date')
 const config = require('./config');
+
+const { dayOfTheWeek, day, month, year } = date();
 
 axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${config.news_API_key}`)
     .then((res) => {
-        console.log(res.data.articles[0]);
+        console.log(res.data.articles);
     })
     .catch((err) => {
         console.log("Error retrieving data: " + err);
     });
+
+console.log(`${dayOfTheWeek} ${month} ${day}, ${year}`);
